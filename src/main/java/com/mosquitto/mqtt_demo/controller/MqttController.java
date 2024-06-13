ed44aea071ca36d156b32b807001f83e4a742fe5
Lucas Giffuni <55067106+LucasGiffuni@users.noreply.github.com>
2024-06-13 18:53:58 -0300

    commit

---
+++ src/main/java/com/mosquitto/mqtt_demo/controller/MqttController.java
@@ -0,0 +1,36 @@
+package com.mosquitto.mqtt_demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mosquitto.mqtt_demo.MqttDemoApplication;
+import com.mosquitto.mqtt_demo.configuration.MqttGateway;
+
+@Controller
+public class MqttController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MqttDemoApplication.class);
+
+    @Autowired
+    MqttGateway gateway;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/sendMqttMessage")
+    public ResponseEntity<?> modifyLightDevice(@RequestParam String message, @RequestParam String topic) {
+        try {
+            logger.info("message" + message);
+            logger.info("topic" + topic);
+            gateway.senToMqtt(message, topic);
+            return ResponseEntity.ok("Success");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return ResponseEntity.ok("fail");
+        }
+    }
+}
